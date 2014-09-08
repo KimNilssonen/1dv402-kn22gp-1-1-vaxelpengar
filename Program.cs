f77c9ffc50babe57a6006863b501e03c988cb294
kn222gp <kn222gp@student.lnu.se>
2014-09-08 15:33:07 +0200

    Lagt till "goto".

--- Program.cs
+++ Program.cs
@@ -12,35 +12,41 @@ namespace Change
         {
             // Skapa variabler.
             double totalSum;
-            double subTotal;
             double roundOff;
+            uint subTotal;
+            uint paid;
+            uint back;
 
-            int paid;
-            int back;
-
-                     
             // Efterfråga totalsumma och erhållet belopp. 
-            Console.Write("Ange totalsumma      : ");
-            totalSum = double.Parse(Console.ReadLine());
+            ask:
 
-            Console.Write("Ange erhållet belopp : ");
-            paid = int.Parse(Console.ReadLine());
+                Console.Write("Ange totalsumma      : ");
+                totalSum = double.Parse(Console.ReadLine());
 
-                if(paid<totalSum)
+                if (Math.Round(totalSum, MidpointRounding.AwayFromZero) < 1)
                 {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Det betalade beloppet är för lite!\n");
-                Console.ResetColor();
-                    return;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Det inmatade beloppet är för litet, köpet kunde inte genomföras!\n");
+                    Console.ResetColor();
+                    goto ask;
                 }
-             
+                Console.Write("Ange erhållet belopp : ");
+                paid = uint.Parse(Console.ReadLine());
+
+                if (paid < totalSum)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Det betalade beloppet är för lite, köpet kunde inte genomföras!\n");
+                    Console.ResetColor();
+                    goto ask;
+                }
+              
 
             // Skapa kvitto, här görs uträkningar och öresavrundning.
             subTotal = (uint)Math.Round(totalSum, MidpointRounding.AwayFromZero);
             roundOff = totalSum - subTotal;
-
             // Beräknar hur mycket köparen får tillbaka.
-            back = paid - (int)subTotal;
+            back = paid - (uint)subTotal;
 
 
 
@@ -54,28 +60,27 @@ namespace Change
             Console.WriteLine(String.Format("Kontant           :{0,15:c}", paid));     // Det betalade beloppet.
             Console.WriteLine(String.Format("Tillbaka          :{0,15:c}\n", back));   // Beloppet du får tillbaka.
             Console.WriteLine("----------------------------------\n");
-
             //String.Format är till för att höger eller vänsterjustera text. Ex {0,10} ger högerjusterat, minusvärde på tian ger istället vänsterjusterat.
             //Jag hittade denna infon här: http://www.csharp-examples.net/align-string-with-spaces/
 
 
-            // Presentera sedlar och mynt.
-            int fiveHundreds = 500;
-            int hundreds = 100;
-            int fifties = 50;
-            int twenties = 20;
-            int tens = 10;
-            int fives = 5;
-            int ones = 1;
-             
 
+            // Presentera sedlar och mynt.
+            uint fiveHundreds = 500;
+            uint hundreds = 100;
+            uint fifties = 50;
+            uint twenties = 20;
+            uint tens = 10;
+            uint fives = 5;
+            uint ones = 1;
+             
             //500
-            int temp = 0;                                           // Gör en temporär int som kan hålla talet efter dividering.
+            uint temp = 0;                                           // Gör en temporär int som kan hålla talet efter dividering.
             temp = back / fiveHundreds;
             back = back % fiveHundreds;                             //Ger antalet femhundralappar. Denna formeln återanvänds för resterande sedlar och mynt.
             if (temp > 0)
             {
-                Console.WriteLine("500 - lappar         :{0}", temp);
+                Console.WriteLine("500 - lappar         :{0}\n", temp);
             }
 
                 //100
@@ -83,7 +88,7 @@ namespace Change
                 back = back % hundreds;
                 if (temp > 0)
                 {
-                    Console.WriteLine("100 - lappar         :{0}", temp);
+                    Console.WriteLine("100 - lappar         :{0}\n", temp);
                 }
 
                     //50
@@ -91,7 +96,7 @@ namespace Change
                     back = back % fifties;
                     if (temp > 0)
                     {
-                        Console.WriteLine("50  - lappar         :{0}", temp);
+                        Console.WriteLine("50  - lappar         :{0}\n", temp);
                     }
 
                         //20
@@ -99,7 +104,7 @@ namespace Change
                         back = back % twenties;
                         if (temp > 0)
                         {
-                            Console.WriteLine("20  - lappar         :{0}", temp);
+                            Console.WriteLine("20  - lappar         :{0}\n", temp);
                         }
                             
                             //10
@@ -107,7 +112,7 @@ namespace Change
                             back = back % tens;
                             if (temp > 0)
                             {
-                                    Console.WriteLine("10  - lappar         :{0}", temp);
+                                Console.WriteLine("10  - lappar         :{0}\n", temp);
                             }
 
                                 //5
@@ -115,7 +120,7 @@ namespace Change
                                 back = back % fives;
                                 if (temp > 0)
                                 {
-                                    Console.WriteLine("5   - kronor         :{0}", temp);
+                                    Console.WriteLine("5   - kronor         :{0}\n", temp);
                                 }
 
                                     //1
@@ -123,7 +128,7 @@ namespace Change
                                     back = back % ones;
                                     if (temp > 0)
                                     {
-                                        Console.WriteLine("1   - kronor         :{0}", temp);
+                                        Console.WriteLine("1   - kronor         :{0}\n", temp);
                                     }
 
         }
